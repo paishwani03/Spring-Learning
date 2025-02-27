f41448326df9e4fd579b4c61bcacc2930572a424
paishwani03 <120698480+paishwani03@users.noreply.github.com>
2025-02-27 12:21:22 +0530

    UC-2

---
+++ helloworld/src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.helloworld.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from BridgeLabz!";
+    }
+}
